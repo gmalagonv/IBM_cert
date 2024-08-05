71124e6b674dbab3cee81a0ea9ec0fea7392b070
Gerard <gemalvalk@gmail.com>
2024-08-05 09:29:26 -0500

    subqueries and nested .sql file updated

--- SQL/M3/Functions_MultipleTables/Sub-queriesandNestedSelects.sql
+++ SQL/M3/Functions_MultipleTables/Sub-queriesandNestedSelects.sql
@@ -43,3 +43,58 @@
           LIMIT 5) AS SALARY_TABLE;
 
 --Note that it is necessary to give an alias to any derived tables.
+
+
+
+
+
+-- Practice Problems
+
+--1) Write a query to find the average salary of the five least-earning employees.
+
+SELECT AVG(SALARY) 
+FROM (SELECT SALARY 
+        FROM EMPLOYEES 
+        ORDER BY SALARY
+        LIMIT 5) AS SALARY_TABLE;
+
+
+
+-- 2) Write a query to find the records of employees older than the average age of all employees.
+
+-- hint: Age in years can be calculated as the year component in the difference between DOB and current date. 
+-- You need to compare the age in years with average age in years. 
+--The average age in years will be evaluated as a sub-query.
+
+-- MY ANSWER:
+    SELECT *
+    FROM EMPLOYEES
+    WHERE DATEDIFF(CURRENT_DATE, B_DATE) > (SELECT AVG(DATEDIFF(CURRENT_DATE, B_DATE)) FROM EMPLOYEES);
+
+-- ANSWER:
+    SELECT * 
+    FROM EMPLOYEES 
+    WHERE YEAR(FROM_DAYS(DATEDIFF(CURRENT_DATE,B_DATE))) > 
+        (SELECT AVG(YEAR(FROM_DAYS(DATEDIFF(CURRENT_DATE,B_DATE)))) 
+        FROM EMPLOYEES);
+
+
+
+--3) From the Job_History table, display the list of Employee IDs, years of service, 
+--and average years of service for all entries.
+--HINT: For this, you need to calculate the years of service as a difference between the date of joining and the current date. 
+--Average years of service need to be queried separately to be displayed. 
+
+-- MY ANSWER:
+SELECT EMPL_ID, YEAR(FROM_DAYS(DATEDIFF(CURRENT_DATE,START_DATE))) AS YRS_SERVICE, 
+        (SELECT AVG(YEAR(FROM_DAYS(DATEDIFF(CURRENT_DATE,START_DATE))))  
+        FROM JOB_HISTORY) AS AVG_YRS_SERVICE
+    FROM JOB_HISTORY;
+
+-- ANSWER:
+SELECT EMPL_ID, YEAR(FROM_DAYS(DATEDIFF(CURRENT_DATE, START_DATE))), 
+    (SELECT AVG(YEAR(FROM_DAYS(DATEDIFF(CURRENT_DATE, START_DATE)))) 
+    FROM JOB_HISTORY)
+FROM JOB_HISTORY;
+
+
